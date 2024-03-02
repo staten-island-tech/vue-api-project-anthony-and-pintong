ca63ba21cc5240557f1f0553c91d7b00ae1ebba6
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2024-03-01 23:10:21 -0500

    demo

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <PokemonCard 
+    v-for="(monster, index) in pokemon" 
+    :key="monster.name"
+    :id="index + 1"
+    :pokemon="monster"
+    />
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted } from 'vue'
+import PokemonCard from "../components/PokemonCard.vue"
+
+
+const pokemon = ref('')
+async function getPokemon() {
+  let res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=35&offset=0")
+  let data = await res.json()
+  pokemon.value = data.results
+}
+onMounted(() => {
+  getPokemon()
+})
+
+</script>
+
+
+
+
+
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/views/PokemonData.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{ monster.name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            monster: {}
+        }
+    },
+    mounted: async function() {
+        await this.getPokemon() 
+    },
+    methods: {
+        getPokemon: async function() {
+            let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`)
+            let data = await res.json()
+            this.monster = data
+            }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
