22a5ebd3d591300bf3785aa6145cc1e0c8e65e3d
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2024-03-11 20:25:47 -0400

    ?

---
+++ vue/src/components/RadarChart.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { Radar } from 'vue-chartjs'
+
+
+
+export default {
+    extends: Radar,
+    data() {
+        return {
+            monster: {}
+        }
+    },
+    mounted: async function() {
+        await this.getPokemon() 
+    },
+    methods: {
+        async function() {
+            const response = await fetch('https://data.cityofnewyork.us/resource/yjub-udmw.json');
+            const data = await response.json();
+            }
+    }
+}
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
